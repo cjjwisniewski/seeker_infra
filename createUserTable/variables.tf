2ac0491257df29a914dc941bf03d790af4303782
Cameron Wisniewski <cjjwisniewski@gmail.com>
2025-03-23 21:33:04 -0400

    remove app function stuff

--- createUserTable/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "subscription_id" {
-  description = "The Azure subscription ID"
-  type        = string
-}
-
-variable "website_run_from_package" {
-  description = "The URL of the package to run from"
-  type        = string
-}
